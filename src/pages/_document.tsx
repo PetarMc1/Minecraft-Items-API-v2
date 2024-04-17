c5d353cd55c429f7756aad21566e412a12872561
Petar_mc <petar.mc.business@gmail.com>
2024-04-17 11:46:38 +0300

    Added keywords, icon and site description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <head>
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <meta name="description" content="Minecraft v2 API for retrieving item and block information."></meta>
+      <meta name="keywords" content="minecraft api, mc api, minecraft api v2, mc api v2, minecraft blocks api, minecraft items api, mc blocks api, mc items api"></meta>
+      <title>Minecraft Items API v2</title>
+      </head>
       <body>
         <Main />
         <NextScript />
