161e3182ada15e8709cba20d7b40091a3ffebe2c
PetarMc1 <160483025+PetarMc1@users.noreply.github.com>
2024-05-02 15:11:41 +0300

    Update site logo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,23 +1,26 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <head>
-        <title>Minecraft API</title>
-        <meta name="description" content="API for retrieving information for Minecraft items, blocks, effects, and other" />
-        <meta name="keywords" content="Minecraft items api, Minecraft api, minecraft blocks API, Petar_mc's API, Minecraft API" />
-        <meta name="author" content="Petar_mc" />
-        {/* Open Graph meta tags */}
-        <meta property="og:title" content="Minecraft API" />
-        <meta property="og:description" content="API for retrieving information for Minecraft items, blocks, effects, and other" />
-        <meta property="og:image" content="/public/favicon.ico" />
-      </head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="description" content="API for retrieving information for Minecraft items, blocks, effects, and other"/>
+          <meta name="keywords" content="Minecraft items API, Minecraft API, minecraft blocks API, Petar_mc's API, Minecraft API"/>
+          <meta name="author" content="Petar_mc"/>
+          <meta property="og:title" content="Minecraft API"/>
+          <meta property="og:description" content="API for retrieving information for Minecraft items, blocks, effects, and other"/>
+          <meta property="og:image" content="https://api.petarmc.com/public/logo.png"/>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
