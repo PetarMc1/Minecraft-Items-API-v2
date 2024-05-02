ce11de70dd40bb2cc1ffa8c5d31c1af663eeaa9c
PetarMc1 <160483025+PetarMc1@users.noreply.github.com>
2024-05-02 15:14:21 +0300

    FIx site logo?

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,7 @@ class MyDocument extends Document {
           <meta name="author" content="Petar_mc"/>
           <meta property="og:title" content="Minecraft API"/>
           <meta property="og:description" content="API for retrieving information for Minecraft items, blocks, effects, and other"/>
-          <meta property="og:image" content="https://api.petarmc.com/public/logo.png"/>
+          <meta property="og:image" content="/public/logo.png"/>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
