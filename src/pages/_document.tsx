d2c7fb383f4868b2baeacce980a929c18d0ea4e1
Petar_mc <160483025+PetarMc1@users.noreply.github.com>
2024-05-04 14:28:52 +0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,14 +5,18 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="title" content="Minecraft Items API v2"/>
           <meta name="description" content="API for retrieving information for Minecraft items, blocks, effects, and other"/>
-          <meta name="keywords" content="Minecraft items API, Minecraft API, minecraft blocks API, Petar_mc's API, Minecraft API"/>
+          <meta name="keywords" content="mcapi, Minecraftapi, Minecraft api, mcapi v2, Minecraft api v2, minecraftapi v2"/>
+          <meta name="robots" content="index, follow"/>
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+          <meta name="language" content="English"/>
           <meta name="author" content="Petar_mc"/>
-          <meta property="og:title" content="Minecraft API"/>
+          <meta property="og:url" content="https://api.petarmc.com"/>
+          <meta property="og:type" content="website"/>
+          <meta property="og:title" content="Minecraft Items API v2"/>
           <meta property="og:description" content="API for retrieving information for Minecraft items, blocks, effects, and other"/>
-          <meta property="og:image" content="/public/logo.png"/>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        	<meta property="og:image" content="https://github.com/PetarMc1/Minecraft-Items-API-v2/blob/ca285895dac643ffd4467d8619102685c33862f2/public/logo.png">
         </Head>
         <body>
           <Main />
